456eadbfa58d6deef6ffc6c791023bb65b32410d
Velez Ezequiel <ezequiel_velez@mcafee.com>
2020-05-20 20:21:20 -0300

    comment 2

--- mitigator/PolicyUtils.cpp
+++ mitigator/PolicyUtils.cpp
@@ -10,7 +10,7 @@
         if (0) { print_error(str(p), GetLastError()); } \
 		    } else
 
-// print mitigation
+// print mitigation function
 void print_mitigations(HANDLE hProc) {
 
 	PROCESS_MITIGATION_DEP_POLICY                       dep = { 0 };
